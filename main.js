3623bcd349bed24cd17192d029d91ad135e65256
gonzalo <gonzaloheizenreder0998@outlook.com>
2026-07-09 00:11:34 -0300

    Trabajado de HTML básico, se agregó de forma muy simple textos del nombre del personaje, vida del mismo y un botón de tirar dado. Ahora cada turno se realiza al presionar el botón de tirar dado

--- main.js
+++ main.js
@@ -12,6 +12,9 @@ let personajes = [
     {id:10, nombre:"Rana", vida:5000, daño:100}
     ];
 
+let jugador;
+let rival;
+
 function dañoDado(){
     let random = Math.floor(Math.random()*8);
     let multiplicador = [0, 1, 2, 3, 4, 5, 6, 7];
@@ -22,55 +25,74 @@ function dañoDado(){
 let transformacion = 0;
 let transformacionRival = 0;
 
-function combate(){
+function eleccionDePjs(personajes){
     let eleccionJugador = prompt("1: Pinguino\n2: Dino\n3: Koala\n4: Babosa\n5: Libélula\n6: Tópo\n7: Quiral\n8: Pepe\n9: Murcielago\n10: Rana");
-    let jugador = personajes[eleccionJugador - 1];
+    jugador = personajes[eleccionJugador - 1];
 
     let eleccionMaquina = Math.floor(Math.random()*personajes.length);
-    let rival = personajes[eleccionMaquina];
-    
-    while((jugador.vida > 0) && (rival.vida > 0)){
-        let golpeJugador = dañoDado();
-        if(golpeJugador === 7){
-            transformacion += 1;
-            console.log(`Acumulador de ${jugador.nombre}: ${transformacion}`);
-            if(transformacion === 3){
-                alert("Evoluciooooooón");
-                rival.vida = 0;
-                console.log(`¡Victoria!`)
-                break;
-            }
-        }
-        else{
-            let dañoFinalJugador = jugador.daño * golpeJugador;
-            rival.vida -= dañoFinalJugador;
-            console.log(`${jugador.nombre} hace ${dañoFinalJugador} de daño. Vida del rival: ${rival.vida}`);
-        }
-        if (rival.vida <= 0) break;
+    rival = personajes[eleccionMaquina];
+    document.querySelector(".statsJugador").innerText = `${jugador.nombre}   vida: ${jugador.vida}`;
+    document.querySelector(".statsRival").innerText = `${rival.nombre}  vida: ${rival.vida}`;
+}
 
-        let golpeRival = dañoDado();
-        if(golpeRival === 7){
-            transformacionRival += 1;
-            console.log(`Acumulador de ${rival.nombre}: ${transformacionRival}`);
-            if(transformacionRival === 3){
-                alert("Evoluciooooooon");
-                jugador.vida = 0;
-                console.log(`¡Derrota!`)
-                break;
-            }
+function combate(){
+
+    // sadsasadasfsaf
+    let tirarDado = document.getElementById("dado");
+    tirarDado.addEventListener("click", (e) =>{
+    let golpeJugador = dañoDado();
+    if(golpeJugador === 7){
+        transformacion += 1;
+        document.querySelector(".acumJugador").innerText = `${transformacion}`;
+        console.log(`Acumulador de ${jugador.nombre}: ${transformacion}`);
+        if(transformacion === 3){
+            alert("Evoluciooooooón");
+            rival.vida = 0;
+            document.querySelector(".statsRival").innerText = `${rival.nombre}  vida: ${rival.vida}`;
+            console.log(`¡Victoria!`)
+            tirarDado.disabled = true;
+            return;
         }
-        else{
-            let dañoFinalRival = rival.daño * golpeRival;
-            jugador.vida -= dañoFinalRival;
-            console.log(`${rival.nombre} hace ${dañoFinalRival} de daño. Vida del Jugador: ${jugador.vida}`);
-        }
+    }
+    else{
+        let dañoFinalJugador = jugador.daño * golpeJugador;
+        rival.vida -= dañoFinalJugador;
+        console.log(`${jugador.nombre} hace ${dañoFinalJugador} de daño. Vida del rival: ${rival.vida}`);
+        document.querySelector(".statsRival").innerText = `${rival.nombre}  vida: ${rival.vida}`;
     }
     if(rival.vida <= 0){
         alert("Victoria");
+        tirarDado.disabled = true;
+        return;
     }
-    else if(jugador.vida <= 0){
-            alert("Perdiste");
+
+    let golpeRival = dañoDado();
+    if(golpeRival === 7){
+        transformacionRival += 1;
+        console.log(`Acumulador de ${rival.nombre}: ${transformacionRival}`);
+        document.querySelector(".acumRival").innerText = `${transformacionRival}`;
+        if(transformacionRival === 3){
+            alert("Evoluciooooooon");
+            jugador.vida = 0;
+            console.log(`¡Derrota!`)
+            document.querySelector(".statsJugador").innerText = `${jugador.nombre}   vida: ${jugador.vida}`
+            tirarDado.disabled = true;
+            return;
         }
+    }
+    else{
+        let dañoFinalRival = rival.daño * golpeRival;
+        jugador.vida -= dañoFinalRival;
+        console.log(`${rival.nombre} hace ${dañoFinalRival} de daño. Vida del Jugador: ${jugador.vida}`);
+        document.querySelector(".statsJugador").innerText = `${jugador.nombre}   vida: ${jugador.vida}`;
+    }
+    if(jugador.vida <= 0){
+        alert("Perdiste");
+        tirarDado.disabled = true;
+        return;
+    }
+    }) 
 }
 
+eleccionDePjs(personajes);
 combate();
